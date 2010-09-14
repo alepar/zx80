d39ff567b63b34a2897d508ec4cdee49a78704df
alepar <alepar@gmail.com>
2010-09-14 14:01:56 +0400

    initial commit

---
+++ src/main/java/ru/alepar/zx80/Main.java
@@ -0,0 +1,12 @@
+package ru.alepar.zx80;
+
+/**
+ * User: alepar
+ * Date: Sep 14, 2010
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
---
+++ src/test/java/ru/alepar/zx80/MainTest.java
@@ -0,0 +1,15 @@
+package ru.alepar.zx80;
+
+import org.junit.Test;
+
+/**
+ * User: alepar
+ * Date: Sep 14, 2010
+ */
+public class MainTest {
+
+    @Test
+    public void dummyTest() {
+        Main.main(null);
+    }
+}
