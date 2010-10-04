d82dc37c2f1d33d8b7699e223833514e44e98810
alepar <alepar@gmail.com>
2010-10-05 00:58:58 +0400

    implemented LdIndexedDst

---
+++ src/main/java/ru/alepar/zx80/op/factory/LdIndexedDst.java
@@ -0,0 +1,47 @@
+package ru.alepar.zx80.op.factory;
+
+import ru.alepar.zx80.base.Cell;
+import ru.alepar.zx80.cpu.WordRegister;
+import ru.alepar.zx80.op.Ld;
+
+import static ru.alepar.zx80.op.factory.LdCommon.*;
+import static ru.alepar.zx80.retrieve.Retrievers.*;
+
+/**
+ * User: alepar
+ * Date: Sep 15, 2010
+ * 
+ * (IX + d) -> A,B,C...
+ */
+public class LdIndexedDst extends SpeccyOpFactory {
+
+    @Override
+    public int accept(Cell[] opcode) {
+        if (opcode.length < 3) {
+            return 0;
+        }
+        if (opcode[0].getValue() == (byte)0xdd || opcode[0].getValue() == (byte)0xfd) {
+            if (getHeader(opcode[1]) == (byte) 0x01) {
+                if (getDstVal(opcode[1]) == (byte) 0x06) {
+                    return 3;
+                }
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public Ld build(Cell[] opcode) {
+        WordRegister r;
+        if (opcode[0].getValue() == (byte) 0xdd) {
+            r = WordRegister.IX;
+        } else {
+            r = WordRegister.IY;
+        }
+        return new Ld(
+                getSrcCell(getSrcVal(opcode[1])),
+                imem(reg(r), opcode[2].getValue())
+            );
+    }
+
+}
---
+++ src/test/java/ru/alepar/zx80/op/factory/LdIndexedDstTest.java
@@ -0,0 +1,42 @@
+package ru.alepar.zx80.op.factory;
+
+import org.junit.Test;
+import ru.alepar.zx80.Speccy;
+import ru.alepar.zx80.SpeccyFactory;
+import ru.alepar.zx80.base.Cell;
+import ru.alepar.zx80.cpu.Register;
+import ru.alepar.zx80.cpu.WordRegister;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static ru.alepar.zx80.base.Address.*;
+
+/**
+ * User: alepar
+ * Date: Oct 3, 2010
+ */
+public class LdIndexedDstTest {
+
+    private final Speccy speccy = SpeccyFactory.buildSpeccy();
+    private final OpFactory opFactory = new LdIndexedDst();
+
+    @Test
+    public void ldIxToAExecutedProperly() {
+        Cell[] opcode = new Cell[]{new Cell(),new Cell(),new Cell()};
+        opcode[0].setValue((byte) 0xfd); // C -> (IY+d)
+        opcode[1].setValue((byte) 0x71);
+        opcode[2].setValue((byte) 0x05);
+
+        assertThat(opFactory.accept(opcode), equalTo(3));
+
+        int addr = 0x10;
+        int offset = 0x05;
+        speccy.getRegistryBlock().getWordRegister(WordRegister.IY).setValue(addr);
+        speccy.getRegistryBlock().getCell(Register.C).setValue((byte) 0xca);
+
+        opFactory.build(opcode).execute(speccy);
+        
+        assertThat(speccy.getMemory().getCell(address(addr+offset)).getValue(), equalTo((byte) 0xca));
+    }
+    
+}
